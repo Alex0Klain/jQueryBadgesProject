8ab1b4f26ea8f0d070d34704e2dc3e799fec0fe3
Alex <alex0klain@gmail.com>
2017-08-26 11:42:14 +0300

    All task

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,8 +4,35 @@ $(function() {
     url: 'https://www.codeschool.com/users/Alex_Klain.json',
     dataType: 'jsonp',
     success: function(response) {
-      $('#badges').html('<div class="course"></div>');
+      addCourse(response.courses.completed);
     }
   });
+
+  function addCourse() {
+
+    var $badges = $('#badges');
+
+    var $course = courses.forEach(function(course){
+      $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+    })
+
+    $('<h3 />', {
+      text: course.title
+    }).appendTo($course);
+
+    $('img', {
+      src: course.bange
+    }).appendTo($course);
+
+    $('<a />', {
+      'class': 'btn btn-primary',
+      target: '_blank',
+      href: course.url,
+      text: 'See course'
+    }).appendTo($course);
+
+  }
 
 });
